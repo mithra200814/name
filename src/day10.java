515c0648a9542ab9c01b0af7b5abf10947bfe3ee
mithra <mithrag25csc@vetias.ac.in>
2026-06-16 15:20:30 +0530

    day10

--- src/day10.java
+++ src/day10.java
@@ -1,9 +1,16 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
+
+//import java.util.LinkedList;
+/*
 public class day10 {
+
+}
+    /*
     static void main() {
-        String[] StudentList = {"nivetha", "narthika", "sweatha","ram","menga"};
-        System.out.println(StudentList.length);
+        //String[] StudentList = {"nivetha", "narthika", "sweatha","ram","menga"};]
         ArrayList<String> list=new ArrayList();
+        //System.out.println(StudentList.length);
+        //ArrayList<String> list=new ArrayList();
         list.add("nivetha");
         list.add("narthika");
         list.add("sweatha");
@@ -13,10 +20,47 @@ public class day10 {
             System.out.println(s);
         }
         list.add("monisha");
-        list.remove("ram");
+        list.remove("ram");                
         System.out.println();
         for(String s:list){
             System.out.println(s);
         }
+    }
+}
+*/
+/*
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("mithra");
+        queue.add("narthika");
+        queue.add("nivetha");
+        queue.add("sweathasri");
+        queue.add("mengasri");
+        while (queue.size()>0) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue) {
+        String removed = queue.removeFirst();
+        System.out.println("Removed:" + queue.remove());
+    }
+}
+*/
+public class day10 {
+    static void main() {
+        LinkedList<String> queue = new LinkedList<String>();
+        queue.add("Sweatha");
+        queue.add("Menaga");
+        queue.add("Narthika");
+        queue.add("Mithra");
+        queue.add("Nivetha");
+        while(!queue.isEmpty()) {
+            process(queue);
+        }
+    }
+    static void process(LinkedList<String> queue){
+        String removed=queue.removeFirst();
+        System.out.println("Removed:"+removed);
+
     }
 }
