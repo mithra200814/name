d407d12fa3c0919739e79afd2558596fe54e4116
mithra <mithrag25csc@vetias.ac.in>
2026-06-17 09:38:19 +0530

    day10

--- src/day10.java
+++ src/day10.java
@@ -46,6 +46,7 @@ public class day10 {
     }
 }
 */
+/*
 public class day10 {
     static void main() {
         LinkedList<String> queue = new LinkedList<String>();
@@ -62,5 +63,61 @@ public class day10 {
         String removed=queue.removeFirst();
         System.out.println("Removed:"+removed);
 
+    }
+}
+*/
+
+import java.util.LinkedList;
+
+public class day10 {
+
+    static class Student {
+        String name;
+        int regNo;
+        int marks;
+
+        Student(String name, int regNo, int marks) {
+            this.name = name;
+            this.regNo = regNo;
+            this.marks = marks;
+        }
+    }
+
+    static void removeStudent(LinkedList<Student> students) {
+        System.out.println("Removed : " + students.removeFirst().name);
+    }
+
+    static void updateStudent(LinkedList<Student> students) {
+        for (Student s : students) {
+            if (s.name.equals("Deepa")) {
+                s.marks = 95;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LinkedList<Student> students = new LinkedList<>();
+
+        students.add(new Student("Arun", 101, 80));
+        students.add(new Student("Bala", 102, 75));
+        students.add(new Student("Charan", 103, 90));
+        students.add(new Student("Deepa", 104, 85));
+        students.add(new Student("Esha", 105, 88));
+
+
+        removeStudent(students);
+
+        updateStudent(students);
+
+
+        System.out.println("Student Details");
+        for (Student s : students) {
+            System.out.println(
+                    s.name + " " +
+                            s.regNo + " " +
+                            s.marks + " "
+            );
+        }
     }
 }
