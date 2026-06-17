35ff77d218d99b5a8885c6f4e2b149b84a0b4b62
mithra <mithrag25csc@vetias.ac.in>
2026-06-17 16:09:08 +0530

    day11

--- src/day11.java
+++ src/day11.java
@@ -1,4 +1,7 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+
+/*import java.util.HashSet;
 import java.util.Set;
 
 public class day11 {
@@ -15,6 +18,24 @@ public class day11 {
     }
 
 }
+*/
+public class day11{
+    public static void main(String[] args){
+       Map<String, Integer> person = new HashMap<>();
+       person.put("ram",89);
+       person.put("john",78);
+       person.put("Aravindh",67);
+       person.put("ramesh",90);
+        System.out.println(person.get("percent"));
+        person.entrySet().forEach(entry ->{
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
+
+}
+
+
+
 
 
 
