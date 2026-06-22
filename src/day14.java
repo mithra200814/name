cd0f87965ccceb9f0c472083f215bf075730d18f
mithra <mithrag25csc@vetias.ac.in>
2026-06-22 12:09:27 +0530

    day14

---
+++ src/day14.java
@@ -0,0 +1,60 @@
+
+public class day14 {
+        static class Student {
+            String name;
+            String email;
+            String phone;
+            int age;
+
+            Student(Builder builder) {
+                this.name = builder.name;
+                this.email = builder.email;
+                this.phone = builder.phone;
+                this.age = builder.age;
+            }
+
+            static class Builder {
+                private String name;
+                private String email;
+                private String phone;
+                private int age;
+
+                public Builder name(String nameValue) {
+                    this.name = nameValue;
+                    return this;
+                }
+
+                public Builder email(String emailValue) {
+                    this.email = emailValue;
+                    return this;
+                }
+
+                public Builder phone(String phoneValue) {
+                    this.phone = phoneValue;
+                    return this;
+                }
+
+                public Builder age(int ageValue) {
+                    this.age = ageValue;
+                    return this;
+                }
+
+                public Student build() {
+                    return new Student(this);
+                }
+            }
+        }
+        static void main(){
+                Student student=new Student.Builder()
+                        .name("Mithra G")
+                        .email("mithraG@email.com")
+                        .phone("2341567890")
+                        .age(18)
+                        .build();
+                System.out.println(student.name);
+                System.out.println(student.email);
+                System.out.println(student.phone);
+                System.out.println(student.age);
+
+    }
+}
